70f5905f09686db7b64f9f09c751cfe81578707d
Bogdan Roatis <aetheern@gmail.com>
2018-01-11 20:58:14 +0200

    Removed unnecessary activity and added fragment for inserting data.

---
+++ app/src/main/java/thejournal/ie/catalog/InsertDataFragment.java
@@ -0,0 +1,62 @@
+package thejournal.ie.catalog;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Bogdan Roatis on 1/11/2018.
+ */
+
+public class InsertDataFragment extends Fragment implements View.OnClickListener{
+    private Button btnAddContact;
+    private TextView tvDisplayContact;
+    private EditText etContactFirstName;
+    private EditText etContactLastName;
+    private EditText etContactNumber;
+    private ContactsDAO contactsDao;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_insert_data, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        contactsDao = new ContactsDAO(getActivity());
+        btnAddContact = view.findViewById(R.id.btn_add_contact);
+        tvDisplayContact = view.findViewById(R.id.tv_contact);
+        etContactFirstName = view.findViewById(R.id.et_contact_first_name);
+        etContactLastName = view.findViewById(R.id.et_contact_last_name);
+        etContactNumber = view.findViewById(R.id.et_contact_number);
+        btnAddContact.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_add_contact:
+                ContactPOJO contact;
+                List<ContactPOJO> contacts;
+                contact = new ContactPOJO(etContactFirstName.getText().toString(), etContactLastName.getText().toString(), etContactNumber.getText().toString());
+                contactsDao.insert(contact);
+                contacts = contactsDao.readContacts();
+                StringBuilder builder = new StringBuilder();
+                for (ContactPOJO c : contacts) {
+                    builder.append(c.getId()).append(" ").append(c.getFirstName()).append(" ").append(c.getLastName()).append(" ").append(c.getPhoneNumber()).append("\n");
+                }
+                tvDisplayContact.setText(builder.toString());
+                break;
+        }
+    }
+}
--- app/src/main/java/thejournal/ie/catalog/MainActivity.java
+++ app/src/main/java/thejournal/ie/catalog/MainActivity.java
@@ -1,57 +1,50 @@
 package thejournal.ie.catalog;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btnAddContact;
-    private Button btnJson;
-    private TextView tvDisplayContact;
-    private EditText etContactFirstName;
-    private EditText etContactLastName;
-    private EditText etContactNumber;
-    private ContactsDAO contactsDao;
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        contactsDao = new ContactsDAO(this);
-        btnAddContact = findViewById(R.id.btn_add_contact);
-        btnJson = findViewById(R.id.btn_json);
-        tvDisplayContact = findViewById(R.id.tv_contact);
-        etContactFirstName = findViewById(R.id.et_contact_first_name);
-        etContactLastName = findViewById(R.id.et_contact_last_name);
-        etContactNumber = findViewById(R.id.et_contact_number);
-        btnAddContact.setOnClickListener(this);
-        btnJson.setOnClickListener(this);
+        switchFragment(new InsertDataFragment());
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.insert_data:
+                        switchFragment(new InsertDataFragment());
+                        break;
+                    case R.id.view_data:
+                        break;
+                    case R.id.location_details:
+                        switchFragment(new LocationFragment());
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_add_contact:
-                ContactPOJO contact;
-                List<ContactPOJO> contacts;
-                contact = new ContactPOJO(etContactFirstName.getText().toString(), etContactLastName.getText().toString(), etContactNumber.getText().toString());
-                contactsDao.insert(contact);
-                contacts = contactsDao.readContacts();
-                StringBuilder builder = new StringBuilder();
-                for (ContactPOJO c : contacts) {
-                    builder.append(c.getId()).append(" ").append(c.getFirstName()).append(" ").append(c.getLastName()).append(" ").append(c.getPhoneNumber()).append("\n");
-                }
-                tvDisplayContact.setText(builder.toString());
-                break;
-            case R.id.btn_json:
-                startActivity(new Intent(this, LocationActivity.class));
-                break;
-        }
+    public void switchFragment(Fragment fragment) {
+        // Create new fragment and transaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+        transaction.replace(R.id.layout_location, fragment);
+        transaction.addToBackStack(null);
+
+// Commit the transaction
+        transaction.commit();
     }
 }
